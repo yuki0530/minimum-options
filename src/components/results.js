bff0d2d98a018a0b5a75c32836e0a8db16d465cc
yuki0530 <0530.y.suzu@gmail.com>
2021-04-18 16:41:11 +0900

    API通信途中

--- src/components/results.js
+++ src/components/results.js
@@ -1,17 +1,47 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import Header from './header';
 
 class results extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false
+        };
+    }
+
+    // componentDidMount() {
+    //     return fetch('https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=c9f39a96973f5b38&large_area=Z011&format=json')
+    //         .then((response) => response.json())
+    //         .then((responseJson) => {
+    //             this.setState({
+    //                 loading: true,
+    //                 data: responseJson,
+    //             });
+    //         })
+    //         .catch((error) => {
+    //             console.error(error);
+    //         });
+    // }
+
+    handleClick(event) {
+        const url = 'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=c9f39a96973f5b38&large_area=Z011&format=json';
+        axios.get(url)
+            .then((res) => { console.log(res); })
+            .catch(console.error);
+    }
+
     render() {
-        return (
+        return(
             <div>
                 <Header />
                 <p>
-                    検索結果
+                    検索結果画面
                 </p>
+                <button onClick={this.handleClick}>Get Data</button>
             </div>
-        );
+        )
     }
 }
 
