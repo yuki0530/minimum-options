d3e26337ac396afaed2c44c93fbda81441e798e3
yuki0530 <0530.y.suzu@gmail.com>
2021-04-25 16:59:18 +0900

    API、CORSエラー

--- src/components/results.js
+++ src/components/results.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 import React, { Component } from 'react';
 import Header from './header';
 
@@ -7,29 +7,31 @@ class results extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading: false
+            
         };
     }
 
     // componentDidMount() {
-    //     return fetch('https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=c9f39a96973f5b38&large_area=Z011&format=json')
-    //         .then((response) => response.json())
-    //         .then((responseJson) => {
-    //             this.setState({
-    //                 loading: true,
-    //                 data: responseJson,
-    //             });
+    //     axios.get(`https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=c9f39a96973f5b38&large_area=Z011&format=json`)
+    //         .then(res => {
+    //             const results = res.data;
+    //             this.setState({ results });
     //         })
-    //         .catch((error) => {
-    //             console.error(error);
-    //         });
     // }
 
     handleClick(event) {
-        const url = 'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=c9f39a96973f5b38&large_area=Z011&format=json';
-        axios.get(url)
-            .then((res) => { console.log(res); })
-            .catch(console.error);
+        fetch('https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=c9f39a96973f5b38&large_area=Z011&format=json', {
+            mode: 'cors',
+            headers: {
+                // 'Content-Type': 'application/json',
+                // 'X-Requested-With': 'XMLHttpRequest',
+                'Access-Control-Allow-Origin': '*',
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            });
     }
 
     render() {
@@ -39,7 +41,7 @@ class results extends Component {
                 <p>
                     検索結果画面
                 </p>
-                <button onClick={this.handleClick}>Get Data</button>
+                <button onClick={this.handleClick}>APIのテスト</button>
             </div>
         )
     }
