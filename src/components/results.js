23199113b651fe49f66a8c88d945dc42d859b94a
yuki0530 <0530.y.suzu@gmail.com>
2021-05-23 14:02:01 +0900

    make it better

--- src/components/results.js
+++ src/components/results.js
@@ -9,13 +9,10 @@ class results extends Component {
 
     handleClick(event) {
         fetch('https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=c9f39a96973f5b38&large_area=Z011&format=json', {
-            method: 'POST',
             mode: 'cors',
             headers: {
                 'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json'
             },
-            body: JSON.stringify(data)
         })
         .then(response => response.json())
         .then(data => {
